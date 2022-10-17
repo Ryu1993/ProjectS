b147563ff61c5dbc63ce92b5ab2bb81ac96c3767
Ryu1993 <fing2106@gmail.com>
2022-10-17 21:06:18 +0900

    fix : Item Scriptabe CreateAsset

--- Assets/Scriptable/Gem.cs
+++ Assets/Scriptable/Gem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName =("Item/Gem"))]
 public class Gem : Item
 {
     [SerializeField]
--- Assets/Scriptable/Slime.cs
+++ Assets/Scriptable/Slime.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName =("Item/Slime"))]
 public class Slime : Item
 {
 
