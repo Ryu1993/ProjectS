aefdd8f37ad68682c25a56b20e1219a1eb362a11
Ryu1993 <fing2106@gmail.com>
2022-10-17 21:11:53 +0900

    feat : IInteraction(흡입),IItemable(아이템화)추가

--- Assets/Scripts/Interface/IInteraction.cs
+++ Assets/Scripts/Interface/IInteraction.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IItemable
+public interface IInteraction
 {
-    public Item ItemRequest();
+    public Transform transform { get; set; }
 
 }
