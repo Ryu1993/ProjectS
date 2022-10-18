a279fdcc5a6d0b74e6f5dc7eee0398aee908dccc
Ryu1993 <fing2106@gmail.com>
2022-10-18 20:05:19 +0900

    feat : MarketConsole 완료

---
+++ Assets/Scripts/Interface/ISaleable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISaleable
+{
+    public void Sale(out Gem gem);
+}
--- Assets/Scripts/Market/MarketConsole.cs
+++ Assets/Scripts/Market/MarketConsole.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class MarketConsole : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private MarketWindow marketWindow;
+    private MarketSlot slot;
+    private bool isActive;
+
+    //MarketSlotÀÇ slotAction(MarketSlotÀÇ TriggerEnter½Ã ¹ß»ýÇÒ °ñµæ È¹µæ ÀÌº¥Æ®)
+    public void Awake()
     {
-        
+        slot.slotAction = (Gem gem)=> GameManager.Instance.playerGold += marketWindow.detectionGems[gem].curPrice;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent(out CapsuleCollider player))
+        {
+            marketWindow.gameObject.SetActive(true);
+            marketWindow.MarketPopUp();
+            slot.SlotSwitch();
+            isActive = true;
+        }   
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        if(isActive)
+        {
+            slot.SlotSwitch();
+            marketWindow.gameObject.SetActive(false);
+            isActive = false;
+        }
     }
 }
