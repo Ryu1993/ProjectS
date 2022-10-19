0bf41b4d1a36f2411d188c0ff30590eb58d9bb80
ChoiByoungChan <3djrahdmrl@gmail.com>
2022-10-19 21:24:29 +0900

    feat: Slime 작성중

---
+++ Assets/Choi/ChoiScript/VacuumPack.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BC
+{
+    public class VacuumPack : MonoBehaviour
+    {
+        private enum STATE { }
+
+    }
+}
+
--- Assets/Scriptable/Slime.cs
+++ Assets/Scriptable/Slime.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName =("Item/Slime"))]
 public class Slime : Item
 {
+    public float hungry;
+    public float speed;
+    public float jumpPower;
     [SerializeField]
     private Gem _rewardGem;
     public Gem rewardGem { get { return _rewardGem; }}
