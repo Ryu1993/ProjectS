e71415d44731f98237e66c256a05b113cd60f364
HanYoungHyeon <112858988+HanYoungHyeon@users.noreply.github.com>
2022-10-20 21:40:14 +0900

    fix objpool
    지저분한 문장 정리

--- Assets/Scripts/yh/ObjectPoolCall.cs
+++ Assets/Scripts/yh/ObjectPoolCall.cs
@@ -5,56 +5,37 @@ using UnityEngine;
 public class ObjectPoolCall : MonoBehaviour
 {
     [SerializeField]
-    GameObject poolTest;
+    GameObject slime;
     ObjectPool objectPool;
     [SerializeField]
-    List<GameObject> fields = new List<GameObject>();
-    [SerializeField]
     List<GameObject> gameObjects = new List<GameObject>();
-    
     public Vector3 spawnPosition;
     [SerializeField]
     int maxAmount;
     public int curAmount;
+
     private void Awake()
     {
         curAmount = 0;
         maxAmount = 50;
-        objectPool = ObjectPoolManager.Instance.PoolRequest(poolTest,maxAmount,3);
+        objectPool = ObjectPoolManager.Instance.PoolRequest(slime, maxAmount, 3);
+    }
+    private void Start()
+    {
         StartCoroutine(CheckAmount());
     }
-    private void Update()
-    {
-       /* if(Input.GetKeyDown(KeyCode.Space))
-        {
-            gameObjects.Add(objectPool.Call(transform.position).gameObject);
-        }*/
-        if(Input.GetKeyDown(KeyCode.Y))
-        {
-            foreach(GameObject go  in gameObjects)
-            {
-                objectPool.Return(go);
-                curAmount--;
-            }
-            gameObjects.Clear();
-        }
-       
 
-    }
-    
     IEnumerator CheckAmount()
     {
-        
+
         while (true)
        {
-            //int i = Random.Range(0, 4);
-            //spawnPosition = fields[i].transform.position;
             if (curAmount < maxAmount)
             {
+                yield return new WaitForSeconds(1f);
                 gameObjects.Add(objectPool.Call(spawnPosition, Quaternion.identity).gameObject);
                 curAmount++;
             }
-            yield return new WaitForSeconds(1f);
         }
     }
 
