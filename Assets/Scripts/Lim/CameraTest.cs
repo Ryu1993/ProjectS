28c8057869e73504b2adaf4b4bbbe4caeec43422
Seungyeon2201 <ksy9387@naver.com>
2022-10-24 15:49:25 +0900

    fix : GetMouseButton 수정
    GetMouseButton 수정

--- Assets/Scripts/Lim/CameraTest.cs
+++ Assets/Scripts/Lim/CameraTest.cs
@@ -27,7 +27,7 @@ public class CameraTest : MonoBehaviour
         {
             transform.position += transform.right * 0.05f;
         }
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity,mask);
