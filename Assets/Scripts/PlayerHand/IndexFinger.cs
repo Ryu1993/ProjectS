584a7838948e6300ff503c695bde0529742202ed
seonu <63259644+bananasplif777@users.noreply.github.com>
2022-10-25 21:19:18 +0900

    fix : market

--- Assets/Scripts/PlayerHand/IndexFinger.cs
+++ Assets/Scripts/PlayerHand/IndexFinger.cs
@@ -7,8 +7,10 @@ public class IndexFinger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("충돌");
         if(other.TryGetComponent(out IStabable target))
         {
+            Debug.Log("충돌2");
             target.StabEvent();
         }
 
