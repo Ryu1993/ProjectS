14e012a0f500e9ad18f152401e3e215f3df57cfe
ChoiByoungChan <3djrahdmrl@gmail.com>
2022-10-25 17:33:29 +0900

    fix : Player스크립트, 슬라임ScroptableObject에 Sprit 변경

--- Assets/Choi/ChoiScript/Player.cs
+++ Assets/Choi/ChoiScript/Player.cs
@@ -31,28 +31,34 @@ namespace BC
         private void Start()
         {
             lineRenderer = GetComponent<LineRenderer>();
+            lineRenderer.enabled = false;
         }
 
         private void Update()
         {
+            //if(Input.GetMouseButton(1))
             if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
             {
                 Time.timeScale = 0.1f;
                 if(Physics.Raycast(leftHand.position, leftHand.forward, out movementHit, Mathf.Infinity, groundMask))
                 {
+                    lineRenderer.enabled = true;
                     linePositions[0] = transform.position + new Vector3(0,+0.75f,0);
                     movePointObject.SetActive(true);
-                    Vector3 dir = movementHit.point - movePointObject.transform.position;
+                    Vector3 dir = movementHit.point -transform.position;
                     objectCharacterController.Move(dir);
                     linePositions[1] = movePointObject.transform.position;
                     lineRenderer.SetPositions(linePositions);
                 }
             }
+            //if(Input.GetMouseButtonUp(1))
             if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
             {
                 Time.timeScale = 1f;
                 transform.position = movePointObject.transform.position + new Vector3(0, 0.75f, 0);
                 movePointObject.SetActive(false);
+                lineRenderer.enabled = false;
+
             }
         }
 
