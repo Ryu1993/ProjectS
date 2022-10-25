7eb11ae0e4e013da922a2d3cfbaf9275ddc7af9d
승빈 임 <imbi9437@gmail.com>
2022-10-26 05:04:39 +0900

    feat : Add Slime Farm Sound and Particle

--- Assets/Scripts/Lim/LimTest.cs
+++ Assets/Scripts/Lim/LimTest.cs
@@ -5,11 +5,41 @@ using UnityEngine;
 public class LimTest : MonoBehaviour
 {
     [SerializeField]
-    private Gem crop;
+    private Crop crop;
+    //Rigidbody rb;
+    private void Awake()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ItemManager.Instance.CreateSceneItem(crop, transform.position);
+        }
+        /*
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up*0.5f,ForceMode.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * 0.5f, ForceMode.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rb.AddForce(Vector3.forward * -0.5f, ForceMode.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.AddForce(Vector3.right * -0.5f, ForceMode.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * 0.5f, ForceMode.Impulse);
+        }
+        */
     }
 
 }
