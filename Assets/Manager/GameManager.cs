e7caff4dabdb157a4f3aff8dc387c4bf5f4ee56d
Ryu1993 <fing2106@gmail.com>
2022-10-27 16:27:03 +0900

    fix : GameManager 보유골드 표기를 위해 수정

--- Assets/Manager/GameManager.cs
+++ Assets/Manager/GameManager.cs
@@ -2,12 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.Events;
 
 public class GameManager : Singleton<GameManager>
 {
     [SerializeField]
     public List<Gem> detectionGems = new List<Gem>();
-    public float playerGold;
+    public float _playerGold;
+    public float playerGold
+    {
+        get { return _playerGold; }
+        set 
+        { 
+            _playerGold = value;
+            goldGetEvent?.Invoke();
+        }
+    }
+    public UnityAction goldGetEvent;
 
     //Gem 획득시 이벤트, 이미 얻은 젬이라면 무시하고 처음 얻는 젬이라면 detectionGems랑 encountGems에 추가 (market에서 사용)
     public void TakeGem(Gem gem)
