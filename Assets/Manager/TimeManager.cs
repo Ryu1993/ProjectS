5a68af2dc8b0dfcae7ffe670a66e1188c10d6ce3
seonu <63259644+bananasplif777@users.noreply.github.com>
2022-10-28 18:17:33 +0900

    fix : realFinal

--- Assets/Manager/TimeManager.cs
+++ Assets/Manager/TimeManager.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class TimeManager : Singleton<TimeManager>
 {
+    [SerializeField]
+    Image screenLight;
     [SerializeField, Range(1,1000)]
     private int timeAcceleration;
     [SerializeField]
-    private int gameSecond = 0;
+    private float gameSecond = 0;
     public int dayCount { get; private set; }
     private WaitForSecondsRealtime realSecond;
     public UnityAction timeProgressAction;
     public UnityAction dayProgressAction;
     public UnityAction halfDayProgressAction;
+    public UnityAction halfhalfDayProgressAction;
     private bool isPause;
 
     private IEnumerator Start()
@@ -24,7 +28,11 @@ public class TimeManager : Singleton<TimeManager>
             if(!isPause)
             {
                 gameSecond += timeAcceleration;
-                if(gameSecond == 720)
+                if(gameSecond%240 == 0)
+                {
+                    halfhalfDayProgressAction?.Invoke();
+                }
+                if(gameSecond%720 == 0)
                 {
                     halfDayProgressAction?.Invoke();
                 }
@@ -35,9 +43,24 @@ public class TimeManager : Singleton<TimeManager>
                     dayProgressAction?.Invoke();
                 }
                 timeProgressAction?.Invoke();
+                ScreenLightSet();
             }
             yield return realSecond;
         }
+    }
+
+    public void ScreenLightSet()
+    {
+        Color nextColor = screenLight.color;
+        if(gameSecond>720)
+        {
+            nextColor.a = (1440 - gameSecond) / 1440 / 1.5f;
+        }
+        else
+        {
+            nextColor.a = gameSecond / 1440 / 1.5f;
+        }
+        screenLight.color = nextColor;
     }
 
 
